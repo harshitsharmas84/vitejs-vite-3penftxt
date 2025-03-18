acc3c277b78786b2cdd9068710a668fefa42382d
Harshit Sharma <harshit.sharma.s84@kalvium.community>
2025-03-18 11:32:17 +0530

    Done

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,15 @@
 // write the book component code here
+import React from "react";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <img src={book.image} alt={book.name} />
+      <h3>{book.name}</h3>
+      <p>{book.genre}</p>
+      <p>{book.author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
